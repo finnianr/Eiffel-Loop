2bc5994ccfde44ea7f3733c197724e1bd7ae9ee6
Finnian Reilly <finnian@eiffel-loop.com>
2026-06-18 14:07:25 +0100

    Reverted suppression of errors from Eiffel Studio

--- test/run/test.sh
+++ test/run/test.sh
@@ -2,6 +2,10 @@
 
 export LANG=en_GB.UTF-8
 export EIFFEL_LOOP=$EIFFEL/library/Eiffel-Loop
-export LD_LIBRARY_PATH="$EIFFEL_LOOP/test/build/$ISE_PLATFORM/package/bin"
+export LD_LIBRARY_PATH=$EIFFEL_LOOP/test/build/$ISE_PLATFORM/package/bin
+
+if [[ ! -f "$LD_LIBRARY_PATH/el_test" ]]; then
+	export LD_LIBRARY_PATH=$EIFFEL_LOOP/test/build/linux-x86-64/EIFGENs/classic/F_code
+fi
 
 $LD_LIBRARY_PATH/el_test $*
